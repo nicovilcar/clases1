78daee42901c44642992946acc48be0b31893be1
nicovilcar <nicovilcar@gmail.com>
2025-09-22 13:31:25 +0000

    cambios

---
+++ clases.py
@@ -0,0 +1,9 @@
+def class perro:
+    def __init__(self, color , eye_color, height, lenght, weight):
+        self.color=color
+        self.eye_color= eye_color
+        self.height= height
+        self.lenght= lenght
+        self.weight= weight
+    
+    def get.per
